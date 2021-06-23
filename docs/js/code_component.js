9397471876e9a30a75f87b6674b1ed1a550fa1ee
fouad.jadouani <fouad.jadouani@total.com>
2021-06-23 23:53:10 +0200

    First commit

---
+++ docs/js/code_component.js
@@ -0,0 +1,62 @@
+(function () {
+  'use strict';
+
+  $('a[href="#"]').click(function (event) {
+    event.preventDefault();
+  })
+
+  $('.bs-component').each(function () {
+    var $component = $(this);
+    var $button = $('<button class="source-button btn btn-primary btn-xs" role="button" tabindex="0">&lt; &gt;</button>');
+    $component.append($button);
+
+    if ($component.find('[data-bs-toggle="tooltip"]').length > 0) {
+      $component.attr('data-html', $component.html());
+    }
+  });
+
+  var sourceModalElem = document.getElementById('source-modal');
+  if (sourceModalElem) {
+    var sourceModal = new bootstrap.Modal(document.getElementById('source-modal'));
+  }
+
+  $('body').on('click', '.source-button', function (event) {
+    event.preventDefault();
+
+    var component = $(this).parent();
+    var html = component.attr('data-html') ? component.attr('data-html') : component.html();
+
+    html = cleanSource(html);
+    html = Prism.highlight(html, Prism.languages.html, 'html');
+    $('#source-modal code').html(html);
+    sourceModal.show();
+  })
+
+  function cleanSource(html) {
+    html = html.replace(/×/g, '&times;')
+      .replace(/«/g, '&laquo;')
+      .replace(/»/g, '&raquo;')
+      .replace(/←/g, '&larr;')
+      .replace(/→/g, '&rarr;')
+
+    var lines = html.split(/\n/);
+
+    lines.shift();
+    lines.splice(-1, 1);
+
+    var indentSize = lines[0].length - lines[0].trim().length;
+    var re = new RegExp(' {' + indentSize + '}');
+
+    lines = lines.map(function (line) {
+      if (line.match(re)) {
+        line = line.slice(Math.max(0, indentSize));
+      }
+
+      return line;
+    });
+
+    lines = lines.join('\n');
+
+    return lines;
+  }
+})();
